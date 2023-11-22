bc78f9cff7cbc35ccbfa7884a1af58b2cad2a73e
ironbound <iron.bound@gmail.com>
2023-11-22 05:35:08 +0100

    fix gym version

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name="envgym",
     version="0.1.0",
-    install_requires=["gymnasium==0.26.0", "pygame==2.1.0"],
+    install_requires=["gymnasium==0.26.3", "pygame==2.1.0"],
 )
